a335e27b08339aface9b141c0d8ff8a5047e5631
Matt Harrison <matt@crosspeak.org>
2025-01-13 09:57:39 -0500

    make it easier to send notifications unencrypted

--- gravityforms-encryption/unencrypted-notifications.php
+++ gravityforms-encryption/unencrypted-notifications.php
@@ -7,42 +7,6 @@
  */
 
 /**
- * Remove the default function from the plugin that triggers the notifications to use placeholders.
+ * Remove the filter which replaces the data in notifications with placeholders.
  */
-remove_filter( 'gform_notification', 'gf_encryption_notification_start', 10, 1 );
-
-/**
- * Only send decrypted notifications if the "to" field of the notification is exactly "secure@example.com".
- *
- * @param array $notification  Notification data.
- * @return array
- */
-function only_certain_email_address( $notification ) {
-	if ( 'secure@example.com' === $notification['to'] ) {
-		return $notification;
-	}
-	global $gf_encryption_sending_notification;
-
-	$gf_encryption_sending_notification = true;
-
-	return $notification;
-}
-add_filter( 'gform_notification', 'only_certain_email_address', 10, 1 );
-
-/**
- * Only send decrypted notifications if the "id" field of the notification is exactly "51794abf1f0d2".
- *
- * @param array $notification  Notification data.
- * @return array
- */
-function only_certain_notification_id( $notification ) {
-	if ( '51794abf1f0d2' === $notification['id'] ) {
-		return $notification;
-	}
-	global $gf_encryption_sending_notification;
-
-	$gf_encryption_sending_notification = true;
-
-	return $notification;
-}
-add_filter( 'gform_notification', 'only_certain_notification_id', 10, 1 );
+remove_filter( 'gform_merge_tag_filter', 'gf_encryption_gform_merge_tag_filter', 10, 4 );
